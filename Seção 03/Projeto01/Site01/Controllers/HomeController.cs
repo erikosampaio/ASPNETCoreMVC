366b47c858332a776274ef4324fb00fec5684c17
Eriko Sampaio <erikoa.93@gmail.com>
2023-03-22 20:33:58 -0300

    MVC - Controller

---
+++ Seção 03/Projeto01/Site01/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new ContentResult() { Content = "Olá Vida!", ContentType = "text/json" };
+        }
+    }
+}
