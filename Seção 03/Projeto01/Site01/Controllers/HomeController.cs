f095d037fdccfa0ec93e850b111958a4b1622251
Eriko Sampaio <erikoa.93@gmail.com>
2023-03-25 15:11:54 -0300

    MVC - Formulário de login.

--- Seção 03/Projeto01/Site01/Controllers/HomeController.cs
+++ Seção 03/Projeto01/Site01/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Site01.Models;
 
 namespace Site01.Controllers
 {
@@ -8,6 +9,32 @@ namespace Site01.Controllers
         {
             // return new ContentResult() { Content = "Olá Vida!", ContentType = "text/json" };
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromForm]Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                if (usuario.Email == "erikoa.93@gmail.com" && usuario.Senha == "123456")
+                {
+                    return Redirect("/palavra");
+                }
+                else
+                {
+                    ViewBag.Message = "Email e/ou senha inválido(s)";
+                    return View();
+                }
+            }
+            else {
+                return View();
+            }
         }
     }
 }
---
+++ Seção 03/Projeto01/Site01/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo {0} é inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        public string Senha { get; set; }
+    }
+}
